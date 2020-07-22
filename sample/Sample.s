f937c7ea1543a44acd2c96552f956a9f7fdf14d7
Dmytro <dm.vict.kr@gmail.com>
2020-07-22 16:11:27 +0300

    Fix sample `Sample.s` use valid call of routine

--- sample/Sample.s
+++ sample/Sample.s
@@ -22,4 +22,26 @@ var ca = _.CommandsAggregator
   commandPrefix : 'node ',
 }).form();
 
-ca.exec();
+/* run first command */
+
+var appArgs = Object.create( null );
+appArgs.subject = '.action.first';
+appArgs.map = { 'action first' : true };
+appArgs.maps = [ appArgs.map ];
+appArgs.subjects = [ '.action.first' ];
+done = 0;
+ca.appArgsPerform({ appArgs : appArgs, allowingDotless : 0 });
+/* log : executable1 */
+
+/* run second command */
+
+var appArgs = Object.create( null );
+appArgs.subject = '.action.second';
+done = 0;
+ca.appArgsPerform({ appArgs : appArgs, allowingDotless : 0 });
+/* log :
+ > node /.../wCommandsAggregator/sample/Action2.s .action.second
+Action2
+ */
+
+
